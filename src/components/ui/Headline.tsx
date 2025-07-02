5500fd3ac749cc6d1b14462b415bd0c483950350
abdullahalnomandev <abdullahalnoman1512@gmail.com>
2025-07-02 20:52:32 +0600

    wa-color-updated

--- src/components/ui/Headline.tsx
+++ src/components/ui/Headline.tsx
@@ -1,9 +1,5 @@
 const Headline = ({ title }: { title: string }) => {
   return (
-    // <h1 className="text-center  font-[cursive] border text-[25px] text-white w-80 m-auto py-1 rounded-full  bg-[#6047ec] shadow-purple-200
-    // ">
-    //   {title}
-    // </h1>
     <h1 className="text-primary text-center font-[cursive]  max-w-[380px] mx-auto py-2 mb-20 shadow-lg shadow-[#a5a4adde] text-2xl rounded-[40px]">
       {title}
     </h1>
--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -18,4 +18,6 @@ const Sidebar = () => {
   );
 };
 
+
+
 export default Sidebar;
